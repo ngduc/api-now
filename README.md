7248dce522142f952cf1fe6b6dce9a500977f1ff
ngduc <ducjava@gmail.com>
2019-09-10 23:04:21 -0700

    feat: initial commit: add commander, json-server;

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # json-man
+
+Based on json-server
---
+++ bin/index.js
@@ -0,0 +1,5 @@
+const program = require('commander');
+const ver = require('../lib/ver');
+
+program.option('-v, --version', 'show version', ver, '')
+program.parse(process.argv);
