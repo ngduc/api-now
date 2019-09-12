5f4d326f3535484493e1d61ea46ab745c83eeb45
ngduc <ducjava@gmail.com>
2019-09-11 21:38:59 -0700

    feat: remove fs-extra; add utils, faker, default dataset;

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,37 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const program = require('commander');
+
 const ver = require('../lib/ver');
+
+const DEFAULT_PORT = 3003;
 
 function collect(value, previous) {
   return previous.concat([value]);
 }
 
 program
-  .usage('[options] <file>')
+  .usage('[options] [file]')
   .description('Launch an API Server to serve data from a JSON or JS file.')
   .option('-c, --cert <certFile>', 'HTTPS cert file', collect, '')
   .option('-k, --key <keyFile>', 'HTTPS key file', collect, '')
-  .option('-p, --port <port>', 'use custom port', collect, '')
-  .option('-v, --version', 'show version', ver, '')
+  .option('-p, --port <port>', 'Use custom port', collect, '')
+  .option('-v, --version', 'Show version', ver, '')
   .option('-w, --watch', 'Watch for changes and reload (.json)', false)
   .action((file, options) => {
     if (!options) {
-      program.help();
+      if (process.argv.length === 2) {
+        require('../lib/server')({
+          port: DEFAULT_PORT,
+          file: ''
+        });
+      }
+      // program.help();
       return;
     }
     const opt = {
-      port: options.port || 3003,
+      port: options.port || DEFAULT_PORT,
       key: options.key,
       cert: options.cert,
       watch: options.watch,
